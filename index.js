7beb7b4dd95497341545fa11c1efaa1299ea95ee
Thomas Pope <tpope0928@gmail.com>
2020-03-23 23:33:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,23 @@ function wakeDog(dogName, dogBreed) {
   concsole.log('Wake ${dogName} the ${dogBreed}');
   return 'Wake ${dogName} the ${dogBreed}'
 }
+
+function leashDog(dogName, dogBreed) {
+  
+}
+
+function walkToPark(dogName, dogBreed) {
+
+}
+
+function throwFrisbee(dogName, dogBreed) {
+
+}
+
+function walkHome(dogName, dogBreed) {
+
+}
+
+function unleashDog(dogName, dogBreed) {
+
+}
